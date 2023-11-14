131cf14b43373e15191cb7c5617385f415ad6db2
Dmitry Tikhomirov <dimasta00@gmail.com>
2023-11-14 14:37:49 +0300

    Changed all quotes to double-quotes

--- hypex/algorithms/__init__.py
+++ hypex/algorithms/__init__.py
@@ -1,4 +1,4 @@
 from hypex.algorithms.faiss_matcher import FaissMatcher
 from hypex.algorithms.no_replacement_matching import MatcherNoReplacement
 
-__all__ = ['FaissMatcher', "MatcherNoReplacement"]
+__all__ = ["FaissMatcher", "MatcherNoReplacement"]
