22f0dba45d85304f27add2f2bbab9fac1b54b344
dmatryus <dmatryus.sqrt49@yandex.ru>
2023-12-25 16:52:30 +0300

    Split ab and aa tests. Bugfix in test/control tagging in aa test. Add args in aa process.

--- hypex/ab_test/__init__.py
+++ hypex/ab_test/__init__.py
@@ -1,3 +1,4 @@
-from hypex.ab_test.ab_tester import AATest, ABTest, merge_groups
+from hypex.ab_test.aa_tester import AATest, merge_groups
+from hypex.ab_test.ab_tester import ABTest
 
 __all__ = ["AATest", "ABTest", "merge_groups"]
--- tests/test_aa.py
+++ tests/test_aa.py
@@ -3,9 +3,10 @@ import pytest
 
 import sys
 from pathlib import Path
+
 sys.path.append(str(Path(".").absolute().parent))
 
-from hypex.ab_test.ab_tester import AATest
+from hypex.ab_test.aa_tester import AATest
 from hypex.utils.tutorial_data_creation import create_test_data
 
 
@@ -26,7 +27,7 @@ def info_col():
 
 def test_aa_simple(data, iterations, info_col):
     model = AATest(target_fields=["pre_spends", "post_spends"], info_cols=info_col)
-    res, datas_dict = model.calc_uniform_tests(data, iterations=iterations)
+    res, datas_dict = model.calc_uniform_tests(data, iterations=iterations, pbar=False)
 
     assert isinstance(res, pd.DataFrame), "Metrics are not dataframes"
     assert res.shape[0] == iterations, (
@@ -48,7 +49,7 @@ def test_aa_group(data, iterations, info_col):
         info_cols=info_col,
         group_cols=group_cols,
     )
-    res, datas_dict = model.calc_uniform_tests(data, iterations=iterations)
+    res, datas_dict = model.calc_uniform_tests(data, iterations=iterations, pbar=False)
 
     assert isinstance(res, pd.DataFrame), "Metrics are not dataframes"
     assert res.shape[0] == iterations, (
@@ -72,7 +73,7 @@ def test_aa_quantfields(data, iterations, info_col):
         group_cols=group_cols,
         quant_field=quant_field,
     )
-    res, datas_dict = model.calc_uniform_tests(data, iterations=iterations)
+    res, datas_dict = model.calc_uniform_tests(data, iterations=iterations, pbar=False)
 
     assert isinstance(res, pd.DataFrame), "Metrics are not dataframes"
     assert res.shape[0] == iterations, (
@@ -94,14 +95,23 @@ def test_aa_process(data, iterations, info_col):
         info_cols=info_col,
         group_cols=group_cols,
     )
-    model.process(data, iterations=iterations, show_plots=False)
+    model.process(data, iterations=iterations, show_plots=False, pbar=False)
 
 
 def test_group_optimization(data, info_col):
     model = AATest(target_fields=["pre_spends", "post_spends"], info_cols=info_col)
-    model.process(data, optimize_groups=True, iterations=5, show_plots=False)
+    model.process(data, optimize_groups=True, iterations=5, show_plots=False, pbar=False)
 
 
-d = create_test_data(rs=52)
-model = AATest(target_fields=["pre_spends", "post_spends"], info_cols="user_id")
-model.process(d, optimize_groups=True, iterations=5, show_plots=True)
+def test_unbalanced_groups(data, iterations, info_col):
+    test_size = 0.3
+    model = AATest(target_fields=["pre_spends", "post_spends"], info_cols=info_col)
+    results = model.process(
+        data,
+        optimize_groups=False,
+        iterations=iterations,
+        show_plots=False,
+        test_size=test_size,
+        pbar=False
+    )
+    assert abs(results["split_stat"]["test %"]/100 - test_size) < 0.05
