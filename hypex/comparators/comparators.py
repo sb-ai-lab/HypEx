992440c9adc19ed1cc37e1a18b2ba0d7b69bdab9
Dmatryus <dmatryus.sqrt49@yandex.ru>
2024-02-27 08:52:15 +0300

    Change base comparator.

--- hypex/comparators/comparators.py
+++ hypex/comparators/comparators.py
@@ -7,22 +7,24 @@ from hypex.dataset.roles import GroupingRole
 from hypex.utils.hypex_typings import FieldKey
 
 # TODO: to Experiment
-class Comparator(Executor):
+class Comparator(ABC, Executor):
     def __init__(
         self,
         target_field: FieldKey,
-        stat: StatDescriptive,
         full_name: str = None,
     ):
         self.target_field = target_field
-        self.comparison_function = comparison_function
         super().__init__(full_name)
+
+    @abstractmethod
+    def _comparison_function(self, control_data, test_data) -> ExperimentData:
+        raise NotImplementedError
 
     def _compare(self, data: ExperimentData) -> bool:
         group_field = data.get_columns_by_roles(GroupingRole)[0]
         grouping_data = list(data.groupby(self.group_field))
         return {
-            grouping_data[i][0]: self.comparison_function(
+            grouping_data[i][0]: self._comparison_function(
                 grouping_data[0][1][self.target_field],
                 grouping_data[i][1][self.target_field],
             )
@@ -41,16 +43,3 @@ class Comparator(Executor):
         compare_result = self._compare(data)
         result_dataset = self._extract_dataset(compare_result)
         return self._set_value(data, result_dataset)
-
-
-class ComparatorInner(ABC, Comparator):
-    @abstractmethod
-    def _comparison_function(self, control_data, test_data) -> ExperimentData:
-        raise NotImplementedError
-
-    def __init__(
-        self,
-        target_field: FieldKey,
-        full_name: str = None,
-    ):
-        super().__init__(target_field, self._comparison_function, full_name)
