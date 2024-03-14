e00e2bb5964c3124d91a06355b604115920ec9d1
Ksu <vasilievaxeniaa@gmail.com>
2024-03-14 11:50:31 +0300

    reformatted pandas_backend structure

--- hypex/dataset/backends/pandas_backend.py
+++ hypex/dataset/backends/pandas_backend.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Sequence, Union, Iterable, List, Dict
+from typing import Sequence, Union, Iterable, List, Dict, Tuple
 
 import pandas as pd
 
@@ -7,6 +7,16 @@ from hypex.dataset.base import DatasetBase
 
 
 class PandasDataset(DatasetBase):
+
+    @staticmethod
+    def _read_file(filename: str) -> pd.DataFrame:
+        file_extension = Path(filename).suffix
+        if file_extension == ".csv":
+            return pd.read_csv(filename)
+        elif file_extension == ".xlsx":
+            return pd.read_excel(filename)
+        else:
+            raise ValueError(f"Unsupported file extension {file_extension}")
 
     def __init__(self, data: Union[pd.DataFrame, Dict, str] = None):
         if isinstance(data, pd.DataFrame):
@@ -22,25 +32,6 @@ class PandasDataset(DatasetBase):
             self.data = self._read_file(data)
         else:
             self.data = None
-
-    @staticmethod
-    def _read_file(filename: str) -> pd.DataFrame:
-        file_extension = Path(filename).suffix
-        if file_extension == ".csv":
-            return pd.read_csv(filename)
-        elif file_extension == ".xlsx":
-            return pd.read_excel(filename)
-        else:
-            raise ValueError(f"Unsupported file extension {file_extension}")
-
-    def _get_column_index(
-        self, column_name: Union[Sequence[str], str]
-    ) -> Union[int, Sequence[int]]:
-        return (
-            self.data.columns.get_loc(column_name)
-            if isinstance(column_name, str)
-            else self.data.columns.get_indexer(column_name)
-        )[0]
 
     def __getitem__(self, item):
         if isinstance(item, (slice, int)):
@@ -58,6 +49,23 @@ class PandasDataset(DatasetBase):
     def _create_empty(self, index=None, columns=None):
         self.data = pd.DataFrame(index=index, columns=columns)
         return self
+
+    def _get_column_index(
+        self, column_name: Union[Sequence[str], str]
+    ) -> Union[int, Sequence[int]]:
+        return (
+            self.data.columns.get_loc(column_name)
+            if isinstance(column_name, str)
+            else self.data.columns.get_indexer(column_name)
+        )[0]
+
+    @property
+    def index(self):
+        return self.data.index
+
+    @property
+    def columns(self):
+        return self.data.columns
 
     def from_dict(self, data: List[Dict], index=None):
         self.data = pd.DataFrame().from_records(data)
@@ -84,18 +92,10 @@ class PandasDataset(DatasetBase):
     def unique(self):
         return self.data.unique()
 
-    @property
-    def index(self):
-        return self.data.index
-
-    @property
-    def columns(self):
-        return self.data.columns
-
     def isin(self, values: Iterable) -> Iterable[bool]:
         return self.data.isin(values)
 
-    def groupby(self, by, axis, **kwargs):
+    def groupby(self, by, axis, **kwargs) -> List[Tuple]:
         groups = self.data[by].unique()
         return [
             (group, self.data[self.data[by] == group].drop(by, axis=1))
