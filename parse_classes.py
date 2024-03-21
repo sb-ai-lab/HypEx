41d4c5fcf3577491151815e057a64a9d0eeb170c
Ksu <vasilievaxeniaa@gmail.com>
2024-03-21 15:55:49 +0300

    more checks for parsing function

--- parse_classes.py
+++ parse_classes.py
@@ -1,40 +1,44 @@
 import ast
-import os
+from pathlib import Path
+
+
+def get_class_attributes(node):
+    attributes = [
+        attr.targets[0].id
+        for attr in node.body
+        if isinstance(attr, ast.Assign) and not attr.targets[0].id.startswith("_")
+    ]
+    print(attributes)
+    for func in node.body:
+        if isinstance(func, ast.FunctionDef) and func.name == "__init__":
+            for call in func.body:
+                if isinstance(call, ast.Assign):
+                    attributes.extend(
+                        [
+                            target.attr
+                            for target in call.targets
+                            if isinstance(target, ast.Attribute)
+                            and target.value.id == "self"
+                            and not target.attr.startswith("_")
+                        ]
+                    )
+    return attributes
+
+
+def parse_file(file_path):
+    with open(file_path, "r") as source_code:
+        tree = ast.parse(source_code.read())
+        return {
+            node.name: get_class_attributes(node)
+            for node in tree.body
+            if isinstance(node, ast.ClassDef)
+        }
 
 
 def parse_directory(directory):
     classes = {}
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".py"):
-                with open(os.path.join(root, file), "r") as source_code:
-                    tree = ast.parse(source_code.read())
-                    for node in tree.body:
-                        if isinstance(node, ast.ClassDef):
-                            attributes = [
-                                attr.targets[0].id
-                                for attr in node.body
-                                if isinstance(attr, ast.Assign)
-                            ]
-
-                            for func in node.body:
-                                if (
-                                    isinstance(func, ast.FunctionDef)
-                                    and func.name == "__init__"
-                                ):
-                                    for call in func.body:
-                                        if isinstance(call, ast.Assign):
-                                            attributes.extend(
-                                                [
-                                                    target.attr
-                                                    for target in call.targets
-                                                    if isinstance(target, ast.Attribute)
-                                                    and target.value.id == "self"
-                                                ]
-                                            )
-
-                            classes[node.name] = attributes
-
+    for file in Path(directory).rglob("*.py"):
+        classes.update(parse_file(file))
     return classes
 
 
