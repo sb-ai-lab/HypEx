8bb7855932ca209123696e6787ca82cb0c00c838
Ksu <vasilievaxeniaa@gmail.com>
2024-03-25 17:31:17 +0300

    changed file names

--- hypex/utils/enums.py
+++ hypex/utils/enums.py
@@ -2,11 +2,11 @@ import enum
 
 
 @enum.unique
-class ExperimentDataEnum(enum.StrEnum):
+class ExperimentDataEnum(enum.Enum):
     additional_fields = "additional_fields"
     analysis_tables = "analysis_tables"
     stats_fields = "stats_fields"
 
 
-class BackendsEnum(enum.StrEnum):
+class BackendsEnum(enum.Enum):
     pandas = "pandas"
--- hypex/utils/typings.py
+++ hypex/utils/typings.py
@@ -1,7 +1,5 @@
 import datetime
-from typing import Any, Union, Dict, List
-
-from hypex.dataset.roles import ABCRole
+from typing import Any, Union
 
 StratificationRoleTypes = Union[float, str, datetime.datetime]
 TreatmentRoleTypes = Union[float, bool, str]
@@ -9,5 +7,3 @@ TargetRoleTypes = Union[float, int, bool]
 ABCRoleTypes = Union[Any]
 
 FieldKey = Union[str, int]
-
-RolesType = Union[Dict[ABCRole, Union[List[str], str]], Dict[str, ABCRole]]
