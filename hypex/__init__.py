c05557e7c572604363df98dc759be88599fe5893
Dmitry Tikhomirov <dimasta00@gmail.com>
2024-03-25 18:08:05 +0300

    Added __version__ attribute

--- hypex/__init__.py
+++ hypex/__init__.py
@@ -2,5 +2,6 @@
 from .ab_test.ab_tester import ABTest
 from .ab_test.aa_tester import AATest
 from .matcher import Matcher
+from .__version__ import __version__
 
 __all__ = ["Matcher", "AATest", "ABTest"]
