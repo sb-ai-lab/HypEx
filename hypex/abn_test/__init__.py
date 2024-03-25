29031b68b6eb1455c50d2439d1a149be24033ce1
Dmitry Tikhomirov <dimasta00@gmail.com>
2024-03-25 17:13:40 +0300

    Moved to abn_test

---
+++ hypex/abn_test/__init__.py
@@ -0,0 +1,5 @@
+from hypex.abn_test.limit_distribution import min_sample_size
+from hypex.abn_test.limit_distribution import quantile_of_marginal_distribution
+from hypex.abn_test.limit_distribution import test_on_marginal_distribution
+
+__all__ = ["min_sample_size", 'quantile_of_marginal_distribution', 'test_on_marginal_distribution']
