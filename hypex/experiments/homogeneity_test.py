f85c2058c2b8611969d3636a24670a5d0fccd564
Ksu <vasilievaxeniaa@gmail.com>
2024-04-08 11:49:15 +0300

    tested

--- hypex/experiments/homogeneity_test.py
+++ hypex/experiments/homogeneity_test.py
@@ -1,20 +1,16 @@
-from hypex.analyzer.aa import OneAASplitAnalyzer
-from hypex.comparators.comparators import GroupDifference, GroupSizes
+from hypex.analyzers.aa import OneAASplitAnalyzer
+from hypex.comparators.comparators import GroupDifference
 from hypex.comparators.hypothesis_testing import TTest, KSTest
 from hypex.dataset.roles import TargetRole, TreatmentRole
-from hypex.experiment.experiment import Experiment, OnRoleExperiment
-from hypex.utils.enums import SpaceEnum
+from hypex.experiments.base import Experiment, OnRoleExperiment
 
 HOMOGENEITY_TEST = Experiment(
     executors=[
         OnRoleExperiment(
             executors=[
-                GroupSizes(grouping_role=TreatmentRole(), space=SpaceEnum.additional),
-                GroupDifference(
-                    grouping_role=TreatmentRole(), space=SpaceEnum.additional
-                ),
-                TTest(grouping_role=TreatmentRole(), space=SpaceEnum.additional),
-                KSTest(grouping_role=TreatmentRole(), space=SpaceEnum.additional),
+                GroupDifference(grouping_role=TreatmentRole()),
+                TTest(grouping_role=TreatmentRole()),
+                KSTest(grouping_role=TreatmentRole()),
             ],
             role=TargetRole(),
         ),
