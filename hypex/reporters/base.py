e5fdc85be792c07bcadb2d8f52fd3588b7aec5cd
Ksu <vasilievaxeniaa@gmail.com>
2024-04-16 17:30:50 +0300

    upd structure

--- hypex/reporters/base.py
+++ hypex/reporters/base.py
@@ -12,7 +12,6 @@ class Reporter(ABC):
     @staticmethod
     def extract_from_one_row_dataset(data: Dataset) -> Dict[str, Any]:
         return {k: v[0] for k, v in data.to_dict()["data"]["data"].items()}
-        
 
     @staticmethod
     def _extract_from_comparators(data: Dataset) -> Dict[str, Any]:
@@ -30,6 +29,7 @@ class Reporter(ABC):
         for i in range(1, len(group_difference)):
             result.update(group_difference[i])
         return result
+
 
 class DictReporter(Reporter, ABC):
     @abstractmethod
