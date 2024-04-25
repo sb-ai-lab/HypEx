96e2fb41e0eceb231c43f3c2f02c711edb37e3e0
Ksu <vasilievaxeniaa@gmail.com>
2024-04-25 12:45:16 +0300

    updated homo test

--- hypex/experiments/homogeneity.py
+++ hypex/experiments/homogeneity.py
@@ -1,11 +1,12 @@
 from hypex.analyzers import OneAASplitAnalyzer
-from hypex.comparators import GroupDifference
+from hypex.comparators import GroupDifference, GroupSizes
 from hypex.comparators import TTest, KSTest
 from hypex.dataset import TargetRole, TreatmentRole
 from hypex.experiments import Experiment, OnRoleExperiment
 
 HOMOGENEITY_TEST = Experiment(
     executors=[
+        GroupSizes(),
         OnRoleExperiment(
             executors=[
                 GroupDifference(grouping_role=TreatmentRole()),
