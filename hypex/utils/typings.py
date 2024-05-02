b3eb04ff41d912c9d5180c09ac3f35635b837e95
Ksu <vasilievaxeniaa@gmail.com>
2024-05-02 13:08:27 +0300

    added some docs into roles structure

--- hypex/utils/typings.py
+++ hypex/utils/typings.py
@@ -2,10 +2,11 @@ import datetime
 from typing import Any, Union, Dict, List
 
 StratificationRoleTypes = Union[float, str, datetime.datetime]
-TreatmentRoleTypes = Union[float, bool, str, int]
+DefaultRoleTypes = Union[float, bool, str, int]
 TargetRoleTypes = Union[float, int, bool]
 CategoricalTypes = str
 
 FieldKey = Union[str, int]
 
 FromDictType = Union[Dict[str, List[Any]], List[Dict[Any, Any]]]
+RoleNameType = str
