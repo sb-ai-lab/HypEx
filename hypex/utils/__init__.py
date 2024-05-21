b927ec4b5e7fa1fb142ceadb61233d0fad755e75
Ksu <vasilievaxeniaa@gmail.com>
2024-05-21 13:08:16 +0300

    added enum with multitest methods

--- hypex/utils/__init__.py
+++ hypex/utils/__init__.py
@@ -1,5 +1,5 @@
 from .constants import ID_SPLIT_SYMBOL, NAME_BORDER_SYMBOL
-from .enums import SpaceEnum, BackendsEnum, ExperimentDataEnum
+from .enums import SpaceEnum, BackendsEnum, ExperimentDataEnum, ABNTestMethodsEnum
 from .errors import (
     SpaceError,
     NoColumnsError,
