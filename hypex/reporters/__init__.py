6dd7f49b0ef81bbf7126408b1d7b01f43551df65
Ksenia <91828862+VasilievaKA@users.noreply.github.com>
2024-07-10 07:34:42 +0000

    review changes

--- hypex/reporters/__init__.py
+++ hypex/reporters/__init__.py
@@ -1,11 +1,7 @@
-# from .aa import AADatasetReporter
-# from .ab import ABDatasetReporter
 from .abstract import Reporter, DictReporter, DatasetReporter
 from .homo import HomoDictReporter, HomoDatasetReporter
 
 __all__ = [
-    # "ABDatasetReporter",
-    # "AADatasetReporter",
     "Reporter",
     "DictReporter",
     "DatasetReporter",
