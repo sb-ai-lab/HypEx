fdeef12e57e0dd604c91b575dc824c5f3396f6d0
Ksenia <91828862+VasilievaKA@users.noreply.github.com>
2024-07-11 12:55:29 +0000

    Added matching tutorial

--- hypex/experiments/twin_search.py
+++ hypex/experiments/twin_search.py
@@ -10,7 +10,6 @@ MATCHING = Experiment(
         PREPROCESSING_DATA,
         MahalanobisDistance(grouping_role=TreatmentRole()),
         FaissNearestNeighbors(grouping_role=TreatmentRole(), two_sides=True),
-        MatchingMetrics(grouping_role=TreatmentRole(), target_roles=[TargetRole()]),
-        SMD(target_roles=[TargetRole()], grouping_role=TreatmentRole()),
+        MatchingMetrics(grouping_role=TreatmentRole(), target_roles=[TargetRole()])
     ]
 )
