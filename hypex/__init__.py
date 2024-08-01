16e97d8bd3a40d19b7913d8958b816c30b7c9a4f
Ksu <vasilievaxeniaa@gmail.com>
2024-08-01 16:16:14 +0300

    added main experiments into external __init__

--- hypex/__init__.py
+++ hypex/__init__.py
@@ -1,1 +1,2 @@
+from hypex.experiments import AATest, ABTest, Matching, HOMOGENEITY_TEST
 from .__version__ import __version__
--- hypex/experiments/__init__.py
+++ hypex/experiments/__init__.py
@@ -1,8 +1,20 @@
+from .aa import AATest
+from .ab import ABTest
 from .base import (
     Experiment,
     OnRoleExperiment,
 )
 from .base_complex import GroupExperiment, CycledExperiment
+from .homogeneity import HOMOGENEITY_TEST
+from .matching import Matching
 
-
-__all__ = ["Experiment", "OnRoleExperiment", "CycledExperiment", "GroupExperiment"]
+__all__ = [
+    "Experiment",
+    "OnRoleExperiment",
+    "CycledExperiment",
+    "GroupExperiment",
+    "AATest",
+    "ABTest",
+    "Matching",
+    "HOMOGENEITY_TEST",
+]
