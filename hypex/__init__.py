44a05952b9b602c98e4dcacacf9636618f4a1fb8
shibanovfv <shibanovfv@yandex.ru>
2025-04-02 23:30:07 +0300

    Очистка

--- hypex/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Tools to configure resources matcher."""
-from .ab_test.ab_tester import ABTest
-from .ab_test.aa_tester import AATest
-from .matcher import Matcher
-from .__version__ import __version__
-
-__all__ = ["Matcher", "AATest", "ABTest"]
--- hypex/algorithms/__init__.py
+++
@@ -1,4 +0,0 @@
-from hypex.algorithms.faiss_matcher import FaissMatcher
-from hypex.algorithms.no_replacement_matching import MatcherNoReplacement
-
-__all__ = ['FaissMatcher', "MatcherNoReplacement"]
