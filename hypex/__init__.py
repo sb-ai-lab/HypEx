6fa2b5af14893abbbfaa9ff16c84ef95068d5e64
anastasiiafed24 <fedorovanasty24@gmail.com>
2025-12-08 19:30:30 +0300

    min_sample_size in extentions

--- hypex/__init__.py
+++ hypex/__init__.py
@@ -3,7 +3,6 @@ from .aa import AATest
 from .ab import ABTest
 from .homogeneity import HomogeneityTest
 from .matching import Matching
-from .extensions import min_sample_size
 
 
-__all__ = ["AATest", "ABTest", "HomogeneityTest", "Matching", "min_sample_size", "__version__"]
+__all__ = ["AATest", "ABTest", "HomogeneityTest", "Matching", "__version__"]
