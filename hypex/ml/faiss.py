6866b125ae7e9f63fffbe14a2aaeb59fbd75d11b
Tony <akatkov89@gmail.com>
2025-12-25 00:17:57 +0300

    comments cleaned

--- hypex/ml/faiss.py
+++ hypex/ml/faiss.py
@@ -72,7 +72,6 @@ class FaissNearestNeighbors(MLExecutor):
                 **kwargs,
             )
             test_data = cls._set_global_match_indexes(test_data, grouping_data[0])
-            # test_data.values[:] = grouping_data[0][1].index.take(test_data.values)
             if two_sides is not True:
                 return {"test": test_data}
             control_data = cls._inner_function(
@@ -83,7 +82,6 @@ class FaissNearestNeighbors(MLExecutor):
                 **kwargs,
             )
             control_data = cls._set_global_match_indexes(control_data, grouping_data[1])
-            # control_data.values[:] = grouping_data[1][1].index.take(control_data.values)
             return {
                 "test": test_data,
                 "control": control_data,
@@ -150,7 +148,6 @@ class FaissNearestNeighbors(MLExecutor):
             test_pairs=self.test_pairs,
         )
         nans = 0
-        # nans = compare_result.isna().sum().get_values(row="sum").sum()
 
         for result in compare_result.values():
             nans += sum(result.isna().sum().get_values(row="sum")) if self.n_neighbors > 1 else result.isna().sum()
@@ -160,15 +157,11 @@ class FaissNearestNeighbors(MLExecutor):
                 f"Faiss returned {nans} nans, which were replaced with dummy matches. Check if the data is suitable for the test.",
                 UserWarning,
             )
-        ds = data.ds.groupby(group_field)
         matched_indexes = Dataset.create_empty()
         for res_k, res_v in compare_result.items():
-            # for i in range(len(compare_result.columns)):
             group = grouping_data[1][1] if res_k == "test" else grouping_data[0][1]
-            t_ds = ds[0][1] if res_k == "test" else ds[1][1]
             t_index_field = (
                 res_v.loc[: len(group) - 1]
-                # .rename({compare_result.columns[i]: "indexes"})
             )
             n_nans = (
                 t_index_field.isna().sum().get_values(row="sum")
@@ -177,11 +170,9 @@ class FaissNearestNeighbors(MLExecutor):
             )
             if any(n_nans):
                 raise PairsNotFoundError
-            # t_index_field = t_index_field.list_to_columns("indexes")
             t_index_field = t_index_field.rename(
                 {col: f"indexes_{i}" for i, col in enumerate(t_index_field.columns)}
             )
-            # t_index_field.columns = ["indexes"] if len(t_index_field.columns) == 1 else [f"indexes_{i}" for i in range(len(t_index_field.columns))]
             matched_indexes = matched_indexes.append(
                 Dataset.from_dict(
                     data={
