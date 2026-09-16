bc99abdd86922caeb617eb4bcfe2b7692e5af721
Eric <ulmaskuloverik@gmail.com>
2026-09-16 15:32:31 +0300

    feat(reporters/aa): deconstruct `report`

--- hypex/reporters/__init__.py
+++ hypex/reporters/__init__.py
@@ -1,5 +1,5 @@
 from .abstract import Reporter, DictReporter, TestDictReporter, DatasetReporter, ResultKey, REPORTABLE_METRICS
-from .aa import AADryTestReporter, AATestReporter, AAPassedReporter, AABestSplitReporter
+from .aa import AATestReporter, AAPassedReporter, AABestSplitReporter
 from .ab import ABTestReporter, CupacReporter
 from .homo import HomogeneityReporter
 from .matching import MatchingReporter, MatchingQualityReporter
@@ -7,7 +7,7 @@ from .matching import MatchingReporter, MatchingQualityReporter
 __all__ = [
     "Reporter", "DictReporter", "TestDictReporter", "DatasetReporter",
     "ResultKey", "REPORTABLE_METRICS",
-    "AADryTestReporter", "AATestReporter", "AAPassedReporter", "AABestSplitReporter",
+    "AATestReporter", "AAPassedReporter", "AABestSplitReporter",
     "ABTestReporter", "CupacReporter",
     "HomogeneityReporter",
     "MatchingReporter", "MatchingQualityReporter",
